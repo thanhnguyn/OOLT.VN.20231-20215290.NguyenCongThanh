d1ea82a1e5907af6d73f53aaadd3f7af9e3eda98
Thanh Nguyen <thnhnguyn761@gmail.com>
2023-10-28 02:07:34 +0700

    Update initial aims

---
+++ Lab02/AimsProject/DigitalVideoDisc.java
@@ -0,0 +1,58 @@
+package Lab02.AimsProject;
+
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+    // Constructor 2: Create a DVD object by category, title, and cost
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    // Constructor 3: Create a DVD object by director, category, title, and cost
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    // Constructor 4: Create a DVD object by all attributes: title, category, director, length, and cost
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+}

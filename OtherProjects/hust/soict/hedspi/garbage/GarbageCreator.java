3dfa6d64f27c874ac3ef42f11b494ce3e5b175fa
Thanh Nguyen <thnhnguyn761@gmail.com>
2023-11-26 12:26:04 +0700

    String, String Builder and StringBuffer

---
+++ OtherProjects/hust/soict/hedspi/garbage/GarbageCreator.java
@@ -0,0 +1,30 @@
+package OtherProjects.hust.soict.hedspi.garbage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GarbageCreator {
+    public static void main(String[] args) {
+        String filename = "text.txt";
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
+        try {
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        startTime = System.currentTimeMillis();
+        String outputString = "";
+        for (byte b : inputBytes) {
+            outputString += (char)b;
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+//		System.out.println(endTime);
+//		System.out.println(startTime);
+        System.out.println(outputString);
+        System.out.println("GarbageCreator");
+    }
+}

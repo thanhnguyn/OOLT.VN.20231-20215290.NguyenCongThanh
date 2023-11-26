13d48cdd91bbec3f99471c1835c3d24aeac3967a
Thanh Nguyen <thnhnguyn761@gmail.com>
2023-11-26 19:22:32 +0700

    Create a complete console application in the Aims class

--- AimsProject/src/hust/soict/hedspi/aims/store/Store.java
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -1,27 +1,28 @@
 package AimsProject.src.hust.soict.hedspi.aims.store;
-import AimsProject.src.hust.soict.hedspi.aims.media.DigitalVideoDisc;
-import AimsProject.src.hust.soict.hedspi.aims.media.Media;
-
 import java.util.ArrayList;
 
+import AimsProject.src.hust.soict.hedspi.aims.media.DigitalVideoDisc;
+
 public class Store {
-
-    private ArrayList<Media> itemsInStore;
-
-    public Store() {
-        this.itemsInStore = new ArrayList<>();
-    }
-
-    public void addMedia(Media dvd) {
+    private ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<DigitalVideoDisc>();
+    private int nbItems = 0;
+    public void addMedia(DigitalVideoDisc dvd) {
         itemsInStore.add(dvd);
-        System.out.println("DVD added to the store: " + dvd.getTitle());
+        nbItems += 1;
+        System.out.println("The disc is added to store!");
     }
-
-    public void removeMedia(Media dvd) {
-        if (itemsInStore.remove(dvd)) {
-            System.out.println("DVD removed from the store: " + dvd.getTitle());
-        } else {
-            System.out.println("DVD not found in the store: " + dvd.getTitle());
+    public void removeMedia(DigitalVideoDisc dvd) {
+        try {
+            itemsInStore.remove(dvd);
+            nbItems -= 1;
+            System.out.println("The disc is removed from store!");
         }
+        catch (Exception e) {
+            System.out.println("Disc doesn't exist!");
+            return ;
+        }
+    }
+    public ArrayList<DigitalVideoDisc> getItemsInStore() {
+        return itemsInStore;
     }
 }
